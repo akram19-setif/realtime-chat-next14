1f9110a240328f911e6ccb4f6ef7954486a71f72
akram19-setif <akramdjidel00@gmail.com>
2025-05-14 13:37:53 +0100

    refactor modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -35,7 +35,9 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
             leaveFrom='opacity-100 translate-y-0 sm:scale-100'
             leaveTo='opacity-0 translate-y-4 sm:translate-y-0  sm:scale-95'
           >
-            {children}
+            <Dialog.Panel>
+              <div />
+            </Dialog.Panel>
           </Transition.Child>
         </div>
       </Dialog>
